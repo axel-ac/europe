fe4c8f3771ccbacc9112e4cde47cb4281e39404e
axel-ac <abdullahceylan.axel@gmail.com>
2023-08-28 16:08:18 +0200

    start

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <span className="text-shadow">European Continent</span>
+    </div>
+  );
+}
+
+export default Header
---
+++ src/helpers/data.js
@@ -0,0 +1,93 @@
+export const data = [
+  {
+    id: 1,
+    title: "Germany",
+    desc: "Germany,officially the Federal Republic of Germany, is a country in the western region of Central Europe. It is the second-most populous country in Europe after Russia, and the most populous member state of the European Union. Germany is situated between the Baltic and North seas to the north, and the Alps to the south. Its 16 constituent states are bordered by Denmark to the north, Poland and the Czech Republic to the east, Austria and Switzerland to the south, and France, Luxembourg, Belgium, and the Netherlands to the west. The nation's capital and most populous city is Berlin and its main financial centre is Frankfurt; the largest urban area is the Ruhr.",
+    image:
+      "https://cdn.pixabay.com/photo/2018/09/16/12/20/woman-church-3681383_640.jpg",
+  },
+  {
+    id: 2,
+    title: "United Kingdom",
+    desc: "What sign is more likely to take a six-hour bath, followed by a luxurious Swedish massage and decadent dessert spread? Why Taurus, of course! Taurus is an earth sign represented by the bull. Like their celestial spirit animal, Taureans enjoy relaxing in serene, bucolic environments surrounded by soft sounds, soothing aromas, and succulent flavors.",
+    image:
+      "https://cdn.pixabay.com/photo/2019/05/23/11/25/windsor-castle-4223726_640.jpg",
+  },
+  {
+    id: 3,
+    title: "France",
+    desc: "Have you ever been so busy that you wished you could clone yourself just to get everything done? That's the Gemini experience in a nutshell. Spontaneous, playful, and adorably erratic, Gemini is driven by its insatiable curiosity. Appropriately symbolized by the celestial twins, this air sign was interested in so many pursuits that it had to double itself. You know, NBD!",
+    image:
+      "https://cdn.pixabay.com/photo/2017/07/13/03/15/paris-2499022_640.jpg",
+  },
+  {
+    id: 4,
+    title: "Italy",
+    desc: "Represented by the crab, Cancer seamlessly weaves between the sea and shore representing Cancer’s ability to exist in both emotional and material realms. Cancers are highly intuitive and their psychic abilities manifest in tangible spaces. But—just like the hard-shelled crustaceans—this water sign is willing to do whatever it takes to protect itself emotionally. In order to get to know this sign, you're going to need to establish trust! ",
+    image:
+      "https://cdn.pixabay.com/photo/2017/10/12/18/09/venice-2845272_640.jpg",
+  },
+  {
+    id: 5,
+    title: "Spain",
+    desc: "Roll out the red carpet because Leo has arrived. Passionate, loyal, and infamously dramatic, Leo is represented by the lion and these spirited fire signs are the kings and queens of the celestial jungle. They're delighted to embrace their royal status: Vivacious, theatrical, and fiery, Leos love to bask in the spotlight and celebrate… well, themselves.",
+    image:
+      "https://cdn.pixabay.com/photo/2022/06/22/16/10/cathedral-7278228_640.jpg",
+  },
+  {
+    id: 6,
+    title: "Poland",
+    desc: "",
+    image:
+      "",
+  },
+  {
+    id: 7,
+    title: "Belgium",
+    desc: "",
+    image:
+      "",
+  },
+  {
+    id: 8,
+    title: "Sweden",
+    desc: "",
+    image:
+      "",
+  },
+  {
+    id: 9,
+    title: "Greece",
+    desc: "",
+    image:
+      "",
+  },
+  {
+    id: 10,
+    title: "Austria",
+    desc: "",
+    image:
+      "",
+  },
+  {
+    id: 11,
+    title: "Switzerland",
+    desc: "",
+    image:
+      "",
+  },
+  {
+    id: 12,
+    title: "Finland",
+    desc: "",
+    image:
+      "",
+  },
+  {
+    id: 13,
+    title: "Norway",
+    desc: "",
+    image:
+      "",
+  },
+];
